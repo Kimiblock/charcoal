4c83e26cb9be844043b411ea094150fe6ec65dde
Kimiblock Moe <kimiblock@icloud.com>
2026-02-13 18:14:50 +0800

    Bump comm

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ var (
 
 /* Special strings may be interpreted
 	private		10.0.0.0 - 10.255.255.255, 172.16.0.0 - 172.31.255.255, 192.168.0.0 - 192.168.255.255 and fd00::/8
-	Custom IPs not supported yet.
+	other strings are resolved to IP addresses
 */
 type appOutPerms struct {
 	//allowIP			[]string
